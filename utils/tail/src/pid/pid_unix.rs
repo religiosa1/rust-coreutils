9947a60d686c3a0ab48b55d70583fc887bf7a48e
Viacheslav Azarov <public@religiosa.ru>
2023-04-27 11:16:48 +0200

    attempted quickfix of unix_pid

--- utils/tail/src/pid/pid_unix.rs
+++ utils/tail/src/pid/pid_unix.rs
@@ -1,4 +1,4 @@
-use nix::sys::signal::{kill, Signal};
+use nix::sys::signal::kill;
 use nix::unistd::Pid;
 
 pub struct PidChecker {
@@ -6,13 +6,13 @@ pub struct PidChecker {
 }
 impl PidChecker {
     pub fn new(pid: u32) -> Result<Self, ()> {
-        Self {
+        Ok(Self {
             pid: Pid::from_raw(pid.try_into().unwrap()),
-        }
+        })
     }
     /** Determining a process status by sending an empty signal */
     pub fn check_pid(&self) -> bool {
-        match kill(self.pid, Signal::null()) {
+        match kill(self.pid, None) {
             Ok(_) => true,
             Err(_) => false,
         }
